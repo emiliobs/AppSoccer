bbbe13431b03b1c3d6a654a0de886e7306c506b7
emilio barrera <barrera_emilio@yahoo.es>
2017-05-03 01:36:32 +0200

    All Login Logic = ok

---
+++ AppSoccer/AppSoccer/Service/DialogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSoccer.Service
+{
+    public  class DialogService
+    {
+        public async Task ShowMessage(string title, string message)
+        {
+            await App.Current.MainPage.DisplayAlert(title, message, "Accept");
+        }
+
+        public async Task<bool> ShowConfirm(string title, string message)
+        {
+            return await App.Current.MainPage.DisplayAlert(title, message, "Yes", "No");
+        }
+
+    }
+
+}
--- AppSoccer/AppSoccer/ViewModels/LoginViewModel.cs
+++ AppSoccer/AppSoccer/ViewModels/LoginViewModel.cs
@@ -1,13 +1,219 @@
-﻿using System;
+﻿using AppSoccer.Models;
+using AppSoccer.Service;
+using GalaSoft.MvvmLight.Command;
+using Plugin.Connectivity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppSoccer.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        #region Atributtes        
+        private ApiService apiService;
+        private DialogService dialogService;
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        private bool isRemembered;
+        #endregion
 
+        #region Properties
+        public string Email
+        {
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+                }
+            }
+            get
+            {
+                return email;
+            }
+        }
+
+        public string Password
+        {
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Password"));
+                }
+            }
+            get
+            {
+                return password;
+            }
+        }
+
+        public bool IsRunning
+        {
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+                }
+            }
+            get
+            {
+                return isRunning;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+            get
+            {
+                return isEnabled;
+            }
+        }
+
+        public bool IsRemembered
+        {
+            set
+            {
+                if (isRemembered != value)
+                {
+                    isRemembered = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRemembered"));
+                }
+            }
+            get
+            {
+                return isRemembered;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            apiService    = new ApiService();
+            dialogService = new DialogService();
+
+            IsEnabled = true;
+            IsRemembered = true;
+
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get { return new RelayCommand(Login); } }
+
+        private async void Login()
+        {
+            //Validaciones de Campos:
+            if (string.IsNullOrEmpty(Email))
+            {
+                await dialogService.ShowMessage("Error", "You must enter the User Email.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await dialogService.ShowMessage("Error", "You must enter the User Password.");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            //Aqui valido la conexión con internet:
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error", "Check your internet connection");
+                return;
+            }
+
+            //Aqui cheqieo si tengo datos, por que puedo tener servicio sin datos(reachable = accesibilidad):
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isReachable)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error","Check Your internet Connection.");
+            }
+
+            var token = await apiService.GetToken("http://soccerapi.azurewebsites.net",Email,Password);
+
+            if (token == null)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error","The user Name or Password ");
+                Password = null;
+                return;
+            }
+
+            //aqui pruebo si no me llega token, aqui muestro el error de mapeo del token tanto arriba como abajo:
+            if (string.IsNullOrEmpty(token.AccessToken))
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error", token.ErrorDescription);
+                Password = null;
+                return;
+
+            }
+
+            //Aqui ya le pregunto con el usurioname o email, para que me retorne el usuario autorizado:
+            var response = await apiService.GetUserByEmail("http://soccerapi.azurewebsites.net", "/api", "/Users/GetUserByEmail",
+                token.TokenType, token.AccessToken, token.UserName);
+
+
+            //aqui valido si el usuario existe(es casi emplosible que el usuario no exista por el token..
+            if (!response.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error", "Problem ocurred retrieving user information, try again latter.");
+                return;
+            }
+
+
+            //Si llego hasta aui todo esta bien..
+            IsRunning = false;
+            IsEnabled = true;
+
+            var user = (User)response.Result;
+
+            await dialogService.ShowMessage("All equals OK...", $"Welcome: {user.FirstName} {user.LastName}, Alias {user.NickName}");
+
+        }
+
+    
+        #endregion
+
+        #region Methods
+
+        #endregion    
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged; 
+        #endregion
     }
 }
