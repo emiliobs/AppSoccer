d999ef4fc9436723fbc03f7b799a6c7946996d98
emilio barrera <barrera_emilio@yahoo.es>
2017-05-06 12:38:19 +0200

    IsRefreshing and RefreshCommand = ok

--- AppSoccer/AppSoccer/ViewModels/SelectTournamentViewModel.cs
+++ AppSoccer/AppSoccer/ViewModels/SelectTournamentViewModel.cs
@@ -1,17 +1,21 @@
 ﻿using AppSoccer.Classes;
 using AppSoccer.Models;
 using AppSoccer.Service;
+using GalaSoft.MvvmLight.Command;
 using Plugin.Connectivity;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AppSoccer.ViewModels
 {
-    public class SelectTournamentViewModel
+    public class SelectTournamentViewModel : INotifyPropertyChanged
     {
 
         #region Atributtes        
@@ -19,11 +23,25 @@ namespace AppSoccer.ViewModels
         private DialogService dialogService;
         private NavigationService navigationService;
         private DataService dataService;
-       
+        private bool isRefreshing;
+
         #endregion
 
         #region Properties
         public ObservableCollection<TournamentItenViewModel> Tournaments { get; set; }
+        public bool IsRefreshing
+        {
+            get {return isRefreshing; } 
+            set
+            {
+                if (isRefreshing != value)
+                {
+                    isRefreshing = value;
+                    //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRefreshing"));
+                   RaiseOnPropertyChange();
+                }
+            }
+}
         #endregion
 
         #region Constructor
@@ -40,6 +58,15 @@ namespace AppSoccer.ViewModels
             LoadTournament();
         }
 
+        #endregion
+
+        #region Command
+        public ICommand RefreshCommand { get { return new RelayCommand(Refresh); } }
+
+        private void Refresh()
+        {
+            LoadTournament();
+        }
         #endregion
 
         #region Methods
@@ -61,9 +88,11 @@ namespace AppSoccer.ViewModels
 
             }
 
+            IsRefreshing = true;
             var parameter = dataService.First<Parameter>(false);
             var user = dataService.First<User>(false);
             var response = await apiService.Get<Tournament>(parameter.URLBase,"/api", "/Tournaments", user.TokenType, user.AccessToken);
+            IsRefreshing = false;
 
             if (!response.IsSuccess)
             {
@@ -93,7 +122,18 @@ namespace AppSoccer.ViewModels
 
 
         }
-                      
+
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaiseOnPropertyChange([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
         #endregion
     }
 }
